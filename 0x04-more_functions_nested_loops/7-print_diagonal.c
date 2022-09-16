ad81faaa1765cb4fdd47f463606565fa05594891
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-16 12:05:04 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal
+ * @n: input
+ * Return: Diagonal
+ */
+void print_diagonal(int n);
+{
+int co, sp;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (co = n; co <= n; co++)
+{
+for (sp = 1; sp < co; sp++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
