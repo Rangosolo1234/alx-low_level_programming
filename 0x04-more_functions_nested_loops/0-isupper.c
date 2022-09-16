6fbb0363261c7c41af1154a8897cc4264fbea1eb
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-16 10:36:44 -0700

    Task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - start point
+ * @c:n  input
+ * Return: 1 uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My loops readme
