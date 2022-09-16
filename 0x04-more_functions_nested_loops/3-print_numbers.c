260f6ed81962f789ff56abd12a3fc332068cd944
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-16 11:18:33 -0700

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9
+ *
+ * Return: 0-9 followed by new line
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
+_putchar('\n');
+}
