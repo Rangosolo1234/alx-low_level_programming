a2af231aff2ce812b767a871a46293183a5b29bc
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-20 04:52:58 -0700

    Task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - character in reverse
+ * @s: input to be changed
+ */
+void rev_string(char *)
+{
+int ini = 0, finish = 0;
+char t;
+
+while (s[finish] != 0)
+finish++;
+finish--;
+
+while (ini < finish)
+{
+t = s[ini];
+s[ini] = s[finish];
+s[finish] = t;
+ini++;
+finish--;
+}
+}
