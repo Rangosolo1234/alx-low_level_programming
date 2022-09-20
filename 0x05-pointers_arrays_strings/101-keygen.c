d7d0a4cdd67813b9665106e135bf7396b08b9626
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-20 14:16:16 -0700

    task11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - 
  * Return: 
  */
 int main(void)
 {
+int pass, sum;
+
+srand(time(NULL));
+sum = 0;
+while (sum <= 2645)
+{
+pass = (rand() % 128);
+sum += pass;
+printf("%c", pass);
+}
+printf("%c", 2772 - sum);
 return (0);
 }
