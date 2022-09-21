58af39d117fba0ca5cbe2debb6402c4e3ae432c5
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-21 01:24:47 -0700

    Task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concantates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to result string
+ */
+ar *_strcat(char *dest, char *src)
+{
+int dlen = 0, i;
+
+while (dest[dlen])
+{
+dlen++;
+}
+for (i = 0; src[i] != 0; i++)
+{
+dest[dlen] = src[i];
+dlen++
+}
+
+dest[dlen] = '\0';
+return (dest);
+}
