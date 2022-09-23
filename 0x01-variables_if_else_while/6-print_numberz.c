528cdcd56c6c2c19a17eb54292eb7ec2e7352147
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-23 14:54:14 -0700

    All tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+
+putchar('\n');
+
+return (0);
+}
