aebfb74432ec2c6c37f63b59f0bfbcc96c03a83d
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-23 12:35:53 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <main.h>
+/**
+ * main - start point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
