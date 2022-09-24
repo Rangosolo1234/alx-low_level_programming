8debf7ea69c57ceea2c4048bd0d18802074ac238
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-09-23 17:41:24 -0700

    Task1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 _putchar("_putchar");
+
 _putchar('\n');
 
 return (0);
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make alpha
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+
+_putchar('\n');
+}
