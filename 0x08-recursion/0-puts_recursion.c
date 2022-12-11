73d15e9755d1dbd045fe54fcb49208bfaa808e5c
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-12-11 04:21:37 -0800

    My first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string followed by new line
+ * @s: string
+ *
+ * Return: on succes 1
+ * On error, -1 returned
+ */
+void _puts_recursion(char *s)
+{
+if (*s == 0)
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s+1);
+}
