9a617a841f98a3440c1c51a094696846300976f7
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-12-13 07:54:11 -0800

    Taks6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,7 +14,7 @@ if (a == b)
 return (1);
 else if (a % b == 0)
 return (0);
-return (prime2(a, b + 1);
+return (prime2(a, b + 1));
 }
 /**
  * is_prime_number - checks number
@@ -27,5 +27,5 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
-return (prime2(n, 2);
+return (prime2(n, 2));
 }
