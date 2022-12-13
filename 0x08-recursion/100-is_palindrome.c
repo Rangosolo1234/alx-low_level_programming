ac19c4e41e6a0d08025e159eb3a64b1ef4cfe1ca
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2022-12-13 09:57:01 -0800

    Task100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,7 +26,7 @@ int palind3(char *a, int l)
 {
 if (*a != *(a + 1))
 return (0);
-else if(*a == 0)
+else if (*a == 0)
 return (1);
 return (palind3(a + 1, l - 2));
 }
@@ -42,5 +42,5 @@ int is_palindrome(char *s)
 int l;
 
 l = palind2(s, 0);
-return (palind3(s, l));
+ireturn (palind3(s, l));
 }
