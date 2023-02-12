2b7476cff26f97595c64af74b628217b6a77a4ae
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-02-11 23:17:55 -0800

    Task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,12 +12,15 @@ int main(int argc, char *argv[])
 {
 int i;
 if (argc == 1)
+{
 printf("%d\n", argc - 1);
+}
 else
 {
 for (i = 0; *argv; i++, argv++)
-;
+{
 printf("%d\n", i - 1);
 }
 Return (0);
+}
 }
