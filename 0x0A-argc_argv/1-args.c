3ec8f907ff8d1797d6ad5a2fe38e05ae1ee9f4b7
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-02-27 23:28:14 -0800

    Task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,22 +5,13 @@
  * main - prints number of arguments passed to main
  * @argc: Number of command line arguments
  * @argv: Array name
+ *
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-int i;
-if (argc == 1)
-{
-printf("%d\n", argc - 1);
-}
-else
-{
-for (i = 0; *argv; i++, argv++)
-{
-printf("%d\n", i - 1);
-}
-Return (0);
-}
+(void) argv;
+printf("%d\n", argc -1);
+return (0);
 }
