c19b44b9106f4bf4ba141217988b379f018fe6e5
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-03-02 04:51:58 -0800

    Tasks malloc freee

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,54 @@
  */
 int main(void)
 {
-char *s;
+#include "notrebloh.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
+ */
+int _strlen(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+{
+}
+return (a);
+}
+
+/**
+ * *_strdup - function to return pointer to string
+ * @str: pointer to string array input
+ * Return: pointer to string created
+ */
+
+char *_strdup(char *str)
+{
+char *ptr;
+int size;
+int x;
+if (str == NULL)
+{
+return (NULL);
+}
+size = _strlen(str) + 1;
+ptr = malloc(size * sizeof(char));
+x = 0;
+while (x < size)
+{
+if (ptr == NULL)
+{
+return (NULL);
+}
+ptr[x] = str[x];
+x++;
+}
+return (ptr);
+}char *s;
 s = _strdup("ALX SE");
 if (s == NULL)
 {
