45d1d29fbfc6c3127c4330161112f04152475f01
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-03-03 03:30:03 -0800

    Task0

---
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - function that allocates memory
+ * @b: the size to allocate
+ *
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
---
+++ 0x0c-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+More malloc to allocate more space
