4287251cfcf2f22a44b052c507b33251fe5bd762
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-03-22 05:12:54 -0700

    Task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,32 +1,25 @@
-/*
- * File: 0-print_list.c
- * Auth: Solomon Kipkirui
- */
-
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_list - print elements of a list
- * @h: Ponter variable to list_t
- *
- * Return: Number of nodes in h
+ * print_list - Function that prints all elements of  list_t
+ * @h: A singly linked list
+ * Return: Integer number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	int coun;
 
-	while (h)
+	for (coun = 0; h != NULL; coun++)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
+			h = h->next;
+			coun++;
+		}
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
-
-	return (nodes);
+	return (coun);
 }
