70a55d4159c1f3d542563aaaf7da92f314394d44
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-03-22 03:10:00 -0700

    Task1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
-	while (h);
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -24,9 +24,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		count++;
-		h->next;
+		nodes++;
+		h = h->next;
 	}
-	return (count);
-}
 
+	return (nodes);
+}
