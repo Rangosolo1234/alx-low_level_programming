0dd8cd9c3f59a6be0bdee2f937ce59046033a618
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-03-22 08:07:06 -0700

    Task100

---
+++ 0x14-bit_manipulation/100-get_endianess.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - Function
+ * Return: 0 for big endian, 1 for little endian
+ */
+
+int get_endianness(void)
+{
+	int col = 1;
+	char *end = (char *) &col;
+
+	if (*end == 1)
+		return (1);
+	return (0);
+}
