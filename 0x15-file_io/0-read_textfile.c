2db9ff88dffd185cbd4985322d7b780d1fcf9a5b
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2023-03-27 01:00:01 -0700

    Task update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - Function that reads a text file and
- * prints it to the POSIX standard output
- *
- * @filename: Pointer
- * @letters: Letters to read and print
- * Return: Number of letters
+ * read_textfile - reads a text file and prints
+ * @filename: variable pointer
+ * @letters: letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: actual number of letters it could read and print, 0 otherwise
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *ltrs = malloc(sizeof(char) * letters);
-	int f_open, f_read, f_write;
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
-	f_open = open(filename, O_RDONLY);
-	if ((f_open == -1) || (ltrs == NULL))
-		return (0);
-	f_read = read(f_open, ltrs, letters);
-	if (f_read == -1)
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
 	{
-		free(ltrs);
+		free(text);
 		return (0);
 	}
-	f_write = write(STDOUT_FILENO, ltrs, f_read);
-	if (f_write == -1)
-	{
-		free(ltrs);
-		return (0);
-	}
-	free(ltrs);
-	close(f_open);
-	return (f_write);
+
+	let = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,32 +1,14 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef FILE_IO
+#define FILE_IO
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_type(int file1, int file2, char *av[]);
-int main(int argc, char *argv[]);
-
-/**
- * ELF Header
-  */
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
 
 #endif
